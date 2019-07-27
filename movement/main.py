4ccad1535771a6b1a3f92b557f9c486ac2aea7be
8613AQ7 <1799853523@qq.com>
2019-07-27 21:22:00 +0800

    exact usage in animation and successfully saved

--- movement/main.py
+++ movement/main.py
@@ -1,8 +1,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.pyplot import quiver
+
+'''
+class quiver198(quiver):
+    def __init__(self, ax, *args,scale=None, headwidth=3, headlength=5, headaxislength=4.5,minshaft=1, minlength=1, units='width', scale_units=None,angles='uv', width=None, color='k', pivot='tail', **kw):
+        super(quiver198, self).__init__(ax, *args,scale, headwidth, headlength, headaxislength,inshaft, minlength, units, scale_units,angles, width, color, pivot, **kw)
+'''
 #a大小始终为1 方向与速度方向垂直
 dt = 0.001
+interval = 100
 sx = []
 sy = []
 axl = []
@@ -60,33 +68,39 @@ def move():
 
 
 
-fig, axx = plt.subplots()
+fig, ax = plt.subplots()
 xdata, ydata = [], []
-ln, = axx.plot([], [], 'b-', animated=False)
-
+line, = ax.plot([], [], 'b-', animated=False)
+point, = ax.plot([],[],'ro')
+arrowv = ax.quiver([],[],[],[],scale = 40,facecolor='deeppink', linewidth=.5)
+arrowa = ax.quiver([],[],[],[], scale = 10,facecolor='green', linewidth=.5)
 def init():
+    # 改变坐标轴及原点显示位置
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.spines['left'].set_position(('data', 0))
 
-    return ln,
-    
-interval = 100
+    # ax.text(0, 0, u'a大小不变 始终与v垂直')
+    ax.axis('equal')
+    ax.set_xlim(min(sx) * 1.5, max(sx) * 1.5)
+    ax.set_ylim(min(sy) * 1.5, max(sy) * 1.5)
+    return line,
+
 def update(frame):
     index = frame * interval
     xdata.append(sx[index])
     ydata.append(sy[index])
-    ln.set_data(xdata, ydata)
+    line.set_data(xdata, ydata)
 
-    arrowv = plt.quiver(sx[index],sy[index],vxl[index],vyl[index], scale = 40,facecolor='deeppink', linewidth=.5)
-    arrowa = plt.quiver(sx[index],sy[index],axl[index],ayl[index], scale = 10,facecolor='green', linewidth=.5)
+    arrowv.set_offsets([sx[index],sy[index]])
+    arrowv.set_UVC(vxl[index],vyl[index])
 
-    point = plt.scatter(sx[index],sy[index],color = 'red')
-    return ln,arrowv,arrowa,point
+    arrowa.set_offsets([sx[index], sy[index]])
+    arrowa.set_UVC(axl[index], ayl[index])
 
-def update2(frame):
-    xdata.append(axl[frame*10])
-    ydata.append(ayl[frame*10])
-    ln.set_data(xdata, ydata)
-    return ln,
-
+    point.set_data(sx[index],sy[index])
+    return line, arrowv, arrowa,point
 
 '''
 dt太小 出现误差 轨迹稍偏离圆
@@ -99,19 +113,8 @@ dt太大 动画太慢
 
 if __name__ == '__main__':
     move()
-    #plt.plot(xl,yl)
-    #改变坐标轴及原点显示位置
-    ax = plt.gca()
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.spines['bottom'].set_position(('data',0))
-    ax.spines['left'].set_position(('data',0))
 
-    #ax.text(0, 0, u'a大小不变 始终与v垂直')
-    plt.axis('equal')
-    plt.xlim(min(sx) * 1.5,max(sx) * 1.5)
-    plt.ylim(min(sy) * 1.5,max(sy) * 1.5)
-    a = list(range(len(sx)//interval))
-    ani = FuncAnimation(fig, update,init_func=init, blit=True,interval = 10,repeat=False)
-    #ani.save('gif.gif',writer='pillow')
-    plt.show()
+    ani = FuncAnimation(fig, update,frames=range(len(sx)//interval),init_func=init, blit=True,interval = 10,repeat=False)
+    ani.save('save.gif',writer = 'pillow')
+    #plt.show()
+
